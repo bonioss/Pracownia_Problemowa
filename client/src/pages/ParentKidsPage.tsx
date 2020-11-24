f1cc9be9bf1f59d4f2f3f60b06c63711517393d2
Matthew Ferry <matthew.ferry007@gmail.com>
2020-11-24 12:30:04 +0100

    added kid avatar to list

--- client/src/pages/ParentKidsPage.tsx
+++ client/src/pages/ParentKidsPage.tsx
@@ -1,7 +1,8 @@
 import {
-  Button, ListItem, ListItemText, styled,
+  Button, ListItem, ListItemText, styled, ListItemAvatar, Avatar,
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import TestIcon from '@material-ui/icons/ChildCare';
 import { useGetMyKid } from 'api/parent';
 import { GenericList } from 'components/GenericList';
 import { GenericListHeader } from 'components/GenericListHeader';
@@ -39,6 +40,11 @@ export const ParentKidsPage = () => {
         )}
         items={(kids.data?.data || []).map(kidGet => (
           <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <TestIcon />
+              </Avatar>
+            </ListItemAvatar>
             <ListItemText primary={`${kidGet.lastName} ${kidGet.firstName}`} />
           </ListItem>
         ))}
